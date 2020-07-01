70ce4e6a07678d95b40cff57edb75b1d8522e731
aimfn <abimanugraha@gmail.com>
2020-07-01 20:37:59 +0700

    Properti Object

--- Object/object.js
+++ Object/object.js
@@ -11,3 +11,19 @@ function Objek() {
 }
 
 Objek()
+
+function Objek2() {
+    var mobil = {
+        type    : 'Sedan',
+        harga   : 200000000,
+        warna   : 'Putih',
+        tahun   : [2000, 2001, 2002, 2003]
+    }
+    
+    console.log(mobil)
+    console.log(mobil.warna)
+    console.log(mobil.type)
+    console.log(mobil.tahun[0])
+}
+
+Objek2()
---
+++ Object/property.js
@@ -0,0 +1,14 @@
+function Properti() {
+    var mobil = {
+        type    : 'Sedan',
+        harga   : 200000000,
+        warna   : 'Putih',
+        tahun   : [2000, 2001, 2002, 2003]
+    }
+    mobil.harga = 100000000; //Merubah properti
+    
+    console.log(mobil)
+
+}
+
+Properti()
